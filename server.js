ad34dbe21d6f5c0a02dd674a4f1c9a1200b49abc
joresalferov2323-cyber <joresalferov2323@gmail.com>
2025-09-07 02:56:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const request = require('request');
+const cors = require('cors');
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get('/proxy', (req, res) => {
-  const cameraUrl = 'http://85.173.234.243/site/embed.html?id=5517&html5';
-  req.pipe(request(cameraUrl)).pipe(res);
+  const targetUrl = 'http://85.173.234.243/site/embed.html?id=5517&html5';
+  req.pipe(request(targetUrl)).pipe(res);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Proxy server running');
+app.listen(PORT, () => {
+  console.log(`Proxy server running on port ${PORT}`);
 });
