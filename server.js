71645469f759035eb90466a84cf5593b2e820ad5
joresalferov2323-cyber <joresalferov2323@gmail.com>
2025-09-07 03:54:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,9 @@
 const express = require('express');
-const request = require('request');
-const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
 
-app.use(cors());
-
-// Прокси: JS-плеер
-app.get('/proxy-js/fpst.flv.js', (req, res) => {
-  const jsUrl = 'http://85.173.234.243/js/fpst.flv.js';
-  req.pipe(request(jsUrl)).pipe(res);
-});
-
-// Прокси: видеопоток FLV
-app.get('/stream', (req, res) => {
-  const streamUrl = 'http://85.173.234.244:9000/rtsp/23606289/c496f32f37858535ac1d';
-  req.pipe(request(streamUrl)).pipe(res);
-});
-
-app.listen(PORT, () => {
-  console.log(`Proxy server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
